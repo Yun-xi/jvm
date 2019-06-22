82234b44611c44c3c59c226efaab5c15f67ec352
XieYaqi <987159036@qq.com>
2019-06-22 11:24:16 +0800

    将自己的类交由扩展累加载器加载

--- src/main/java/com/xxxx/jvm/classloader/MyTest22.java
+++ src/main/java/com/xxxx/jvm/classloader/MyTest22.java
@@ -2,7 +2,7 @@ package com.xxxx.jvm.classloader;
 
 /**
  * java -Djava.ext.dirs=./ com.xxxx.jvm.classloader.MyTest22
- * 将当前目录作为扩展类加载器加载的目录，得出
+ * 将当前目录作为扩展类加载器加载的目录，输出
  * MyTest22 initializer
  * sun.misc.Launcher$AppClassLoader@73d16e93
  * sun.misc.Launcher$AppClassLoader@73d16e93
@@ -10,8 +10,12 @@ package com.xxxx.jvm.classloader;
  * 发现MyTest22并没有由扩展类加载器来加载，原因是因为扩展类加载器只会加载jar
  *
  * 先把MyTest1.class打成jar包 jar test.jar com/xxxx/jvm/classloader/MyTest1.class
+ * 再执行，输出
+ * MyTest22 initializer
+ * sun.misc.Launcher$AppClassLoader@2a139a55
+ * sun.misc.Launcher$ExtClassLoader@3d4eac69
  *
- * 再执行
+ * 可以看到这时候，MyTest1就由扩展累加载器加载了
  *
  */
 public class MyTest22 {
