492790dcb8823b666be5cad1a79b71177f97782e
XieYaqi <987159036@qq.com>
2019-06-26 23:23:42 +0800

    JDBC

--- src/main/java/com/xxxx/jvm/classloader/MyTest27.java
+++ src/main/java/com/xxxx/jvm/classloader/MyTest27.java
@@ -6,6 +6,7 @@ import java.sql.DriverManager;
 public class MyTest27 {
 
     public static void main(String[] args) throws Exception {
+        System.out.println(System.getProperty("jdbc.drivers"));
         Class.forName("com.mysql.jdbc.Driver");
 
         Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mytestdb", "username", "password");
